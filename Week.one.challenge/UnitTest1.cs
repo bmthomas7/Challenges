dc33d8ee556fc0f71461406a4870959312ac81af
Ben <bmthomas7@gmail.com>
2020-11-17 19:49:35 -0500

    third commit

--- Week.one.challenge/UnitTest1.cs
+++ Week.one.challenge/UnitTest1.cs
@@ -7,6 +7,66 @@ namespace Week.one.challenge
     public class UnitTest1
     {
         [TestMethod]
+        public void NameAndAge()
+        {
+            string firstName = "Ben";
+            string lastName = "Thomas";
+            int age = 23;
+            Console.WriteLine(age + 7);
+            Console.WriteLine(age - 7);
+            Console.WriteLine(age * 7);
+            Console.WriteLine(age / 7);
+            Console.WriteLine(age % 7);
+
+        }
+
+        [TestMethod]
+        public void Array()
+        {
+            string[] movie = { "Avatar", "frozen", "inception", "moana" };
+        }
+
+        [TestMethod]
+        public void ListOfDates()
+        {
+            List<DateTime> listOfDates = new List<DateTime>();
+            DateTime today = DateTime.Today;
+            listOfDates.Add(today);
+            listOfDates.Add(new DateTime(2020, 11, 25));
+            listOfDates.Add(new DateTime(2020, 4, 11));
+            listOfDates.Add(new DateTime(2020, 30, 5));
+
+
+        }
+
+        [TestMethod]
+        public void HowMuchSleepDidYouGet()
+        {
+            Console.WriteLine("how many hours of sleep did you get?");
+            int hours = 4;
+            if (hours >= 10)
+            {
+                Console.WriteLine("you got a ton of sleep");
+            }
+            else if (hours >= 8 && hours < 10) 
+            {
+                Console.WriteLine("that is a good amount of sleeo");
+            }
+            else if (hours >= 4 && hours < 8)
+            {
+                Console.WriteLine("that is about as much sleep as i usually get");
+            }
+            else
+            {
+                Console.WriteLine("get some sleep!");
+            }
+
+
+
+        }
+
+
+        [TestMethod]
         public void HowHasYourDayBeen()
         {
             string userDay = "Okay";

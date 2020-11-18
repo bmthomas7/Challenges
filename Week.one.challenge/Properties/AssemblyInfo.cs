764a37fa8bc4cab1dd2a4ce6085b88483bdae3fa
Ben <bmthomas7@gmail.com>
2020-11-17 19:02:40 -0500

    initial commit

---
+++ Week.one.challenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week.one.challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week.one.challenge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e530beaf-1a14-4fde-9adc-523417e51f4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week.one.challenge/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week.one.challenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HowHasYourDayBeen()
+        {
+            string userDay = "Okay";
+            switch (userDay)
+            {
+                case "great":
+                    Console.WriteLine("Glad you are having a great day");
+                    break;
+                case "good":
+                    Console.WriteLine("it is a day to be good on");
+                    break;
+                case "okay":
+                    Console.WriteLine("hopefully it will get better from here");
+                    break;
+                case "bad":
+                    Console.WriteLine("there is always tomorrow");
+                    break;
+                case ":(":
+                    Console.WriteLine("I guess this is us now");
+                    break;
+                default:
+                    Console.WriteLine("please enter something above");
+                    break;
+            }
+        }
+        [TestMethod]
+        public void Supercalifragilisticexpialidocious()
+        {
+            string name = "supercalifragilisticexpialidocious";
+            foreach(char letter in name)
+            {
+                if ( letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("not an i");
+                }
+            }
+        }
+    }
+}
